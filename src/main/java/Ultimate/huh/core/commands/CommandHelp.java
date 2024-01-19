b90f5181e28347827453578e75cb23ab54afa1ec
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-01-20 03:29:24 +0800

    update plugin

--- src/main/java/Ultimate/huh/core/commands/CommandHelp.java
+++ src/main/java/Ultimate/huh/core/commands/CommandHelp.java
@@ -1,6 +1,7 @@
 package Ultimate.huh.core.commands;
 
 import Ultimate.huh.core.UltimateRPGPlugin;
+import Ultimate.huh.core.commands.impl.CommandDescription;
 import Ultimate.huh.core.commands.impl.URPGCommandsFactory;
 import org.bukkit.command.CommandSender;
 import org.jetbrains.annotations.NotNull;
@@ -14,10 +15,13 @@ public class CommandHelp extends URPGCommandsFactory {
     }
 
     private static UltimateRPGPlugin instance;
+    private static CommandDescription cd;
 
     public void URPGCommand(@NotNull UltimateRPGPlugin plugin, @NotNull CommandSender sender, @NotNull String alias, @NotNull @Unmodifiable List<String> params) {
         String[] args = params.toArray(new String[params.size()]);
         switch (args[1]){
+            case "":
+
             case "help":
                 sender.sendMessage("");
                 break;
@@ -40,6 +44,7 @@ public class CommandHelp extends URPGCommandsFactory {
 
                 break;
             case "announcement":
+
 
                 break;
         }
---
+++ src/main/java/Ultimate/huh/core/commands/impl/CommandDescription.java
@@ -0,0 +1,14 @@
+package Ultimate.huh.core.commands.impl;
+
+public enum CommandDescription {
+    name,
+    description,
+    ;
+
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
+}
+
