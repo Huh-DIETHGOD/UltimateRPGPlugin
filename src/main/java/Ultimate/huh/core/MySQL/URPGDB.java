15930f84100e4f24449c784aecf8dc5a856848e1
Huh <m82b1@qq.com>
2024-03-03 22:17:25 +0800

    Create URPGDB.java

---
+++ src/main/java/Ultimate/huh/core/MySQL/URPGDB.java
@@ -0,0 +1,22 @@
+package Ultimate.huh.core.MySQL;
+
+import cc.carm.lib.easysql.api.SQLManager;
+import cc.carm.lib.easysql.api.SQLTable;
+import cc.carm.lib.easysql.api.builder.TableCreateBuilder;
+import cc.carm.lib.easysql.api.enums.IndexType;
+import cc.carm.lib.easysql.api.enums.NumberType;
+import cc.carm.lib.easysql.api.table.NamedSQLTable;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+
+import java.sql.SQLException;
+import java.util.function.Consumer;
+
+public class URPGDB{
+  
+
+
+
+
+  
+}
