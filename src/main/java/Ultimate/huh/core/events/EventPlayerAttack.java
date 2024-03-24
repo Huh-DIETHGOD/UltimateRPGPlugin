8932cea5949326f5d7533f15aaf9c0e32520a566
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-03-24 16:55:03 +0800

    update command

---
+++ src/main/java/Ultimate/huh/core/events/EventPlayerAttack.java
@@ -0,0 +1,11 @@
+package Ultimate.huh.core.events;
+
+import jdk.jfr.Event;
+
+public class EventPlayerAttack extends Event {
+    public EventPlayerAttack (  ) {
+
+
+
+    }
+}
---
+++ src/main/java/Ultimate/huh/core/state/intellgence/Intelligence.java
@@ -0,0 +1,45 @@
+package Ultimate.huh.core.state.intellgence;
+
+import org.bukkit.entity.Player;
+
+import java.awt.*;
+import java.util.HashMap;
+
+public class Intelligence {
+    private Player player;
+    private float intelligence;
+    private String icon = "❁";
+    private HashMap map;
+
+    public Intelligence(){
+
+
+
+
+
+        player;
+
+
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    public float getIntelligence() {
+        return intelligence;
+    }
+
+    public void setIntelligence(float intelligence) {
+        this.intelligence = intelligence;
+    }
+}
