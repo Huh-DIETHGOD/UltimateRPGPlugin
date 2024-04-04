b4dc152ddc4a75a15704dadcfab104dca040ed8d
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-04-04 13:01:52 +0800

    LanguageSetting update

--- src/main/java/Ultimate/huh/core/config/language/LanguageSetting.java
+++ src/main/java/Ultimate/huh/core/config/language/LanguageSetting.java
@@ -4,17 +4,23 @@ import Ultimate.huh.core.UltimateRPGPlugin;
 import org.bukkit.configuration.file.FileConfiguration;
 
 public class LanguageSetting {
-    private String language = "English";
+
+    private String language;
     private static UltimateRPGPlugin instance;
     FileConfiguration config = instance.getConfig();
 
-    public String getLanguage() {
-        return language;
+    public void Language(String language) {
+        language = config.getString("Ultimate.language.language");
+        if (language == null) {
+            config.set("Ultimate.language.language", "English");
+        }
     }
 
-    public void setLanguage(String language) {
-        if (language != null) {
-            config.set("Ultimate.language.language", language);
-        }
+    public static void setInstance(UltimateRPGPlugin instance) {
+        LanguageSetting.instance = instance;
+    }
+
+    public String getLanguage() {
+        return language;
     }
 }
