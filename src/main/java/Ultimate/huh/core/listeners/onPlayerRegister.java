6a7eb99ed114c83e6e1b4bdc2c463d02beb39489
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-04-30 23:30:18 +0800

    normal update

--- src/main/java/Ultimate/huh/core/listeners/onPlayerRegister.java
+++ src/main/java/Ultimate/huh/core/listeners/onPlayerRegister.java
@@ -1,15 +1,20 @@
 package Ultimate.huh.core.listeners;
 
 import Ultimate.huh.core.events.EventPlayerLogin;
+import cc.carm.lib.easysql.api.SQLManager;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.player.PlayerLoginEvent;
 
+import java.sql.SQLException;
 import java.util.EventListener;
 
 public class onPlayerRegister implements EventListener {
+    SQLManager sqlManager;
     @EventHandler
-    public void onPlayerRegister(PlayerLoginEvent event) {
-
+    public void onPlayerRegister(PlayerLoginEvent event) throws SQLException {
+        sqlManager.getConnection();
+        sqlManager.createInsert("URPGTABLE")
+                .setColumnNames();
 
 
     }
