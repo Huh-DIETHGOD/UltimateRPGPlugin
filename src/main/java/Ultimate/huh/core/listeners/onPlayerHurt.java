68032645a82c35ebad1976628267c7506a661df3
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-05-02 23:20:01 +0800

    normal update

--- src/main/java/Ultimate/huh/core/listeners/onPlayerHurt.java
+++ src/main/java/Ultimate/huh/core/listeners/onPlayerHurt.java
@@ -1,15 +1,30 @@
 package Ultimate.huh.core.listeners;
 
 
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.Player;
+import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
 
 public class onPlayerHurt implements Listener {
+    private static double damage;
+    private Player player;
+    private Entity damageDealer;
     @EventHandler
     public void onPlayerHurt(EntityDamageByEntityEvent event){
+        damage = event.getDamage();
+        damageDealer = event.getDamager();
+        player = (Player) event.getEntity();
+    }
 
+    public static double getDamage() {
+        return damage;
+    }
 
+    public static void setDamage(double damage) {
+        onPlayerHurt.damage = damage;
     }
 
 
