f30d3e52c6eb2aac255acba94547ffb91fdf7b3f
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-05-12 01:53:53 +0800

    injector

---
+++ src/main/java/Ultimate/huh/external/injector/injector.java
@@ -0,0 +1,15 @@
+package Ultimate.huh.external.injector;
+
+import java.nio.Buffer;
+
+/**
+ * This function is used to inject files to the server core
+ *
+ */
+public class injector {
+    private Buffer buffer;
+
+
+
+
+}
---
+++ src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
@@ -0,0 +1,10 @@
+package Ultimate.huh.external.replacefiles.terminal;
+
+public class originalTerminalBlocker {
+    public static void TerminalBlocker(){
+
+
+
+
+    }
+}
