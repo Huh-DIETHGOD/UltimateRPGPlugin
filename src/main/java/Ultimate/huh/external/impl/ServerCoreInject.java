b1151635cd828a58055a6d17ce7e0c22ca94e319
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-06-01 10:19:07 +0800

    injector

--- src/main/java/Ultimate/huh/external/impl/ServerCoreInject.java
+++ src/main/java/Ultimate/huh/external/impl/ServerCoreInject.java
@@ -1,10 +1,11 @@
 package Ultimate.huh.external.impl;
 
 import java.io.File;
+import java.io.Writer;
 
 public interface ServerCoreInject {
-    File open();
-    File write();
+    File open(File file);
+    File write(File targetFile, Writer writer);
 
 
 }
--- src/main/java/Ultimate/huh/external/injector/Injector.java
+++ src/main/java/Ultimate/huh/external/injector/Injector.java
@@ -1,5 +1,7 @@
 package Ultimate.huh.external.injector;
 
+import Ultimate.huh.external.impl.ServerCoreInject;
+
 import java.awt.*;
 import java.io.File;
 import java.io.Writer;
@@ -9,7 +11,7 @@ import java.nio.Buffer;
  * This function is used to inject files to the server core
  *
  */
-public class Injector {
+public class Injector implements ServerCoreInject {
     private Buffer buffer;
     private Writer writer;
     private File serverCore = new File("");
@@ -18,4 +20,13 @@ public class Injector {
 
     }
 
+    @Override
+    public File open(File file) {
+        return serverCore;
+    }
+
+    @Override
+    public File write(File targetFile, Writer writer) {
+        return null;
+    }
 }
