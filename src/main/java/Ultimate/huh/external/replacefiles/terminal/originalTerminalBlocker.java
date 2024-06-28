548524d1eabf499dbad8e6672adace89edce9def
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-06-28 08:21:50 +0800

    term update

--- src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
+++ src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
@@ -1,6 +1,15 @@
 package Ultimate.huh.external.replacefiles.terminal;
 
-public class originalTerminalBlocker {
+import javax.swing.*;
+import java.awt.*;
+
+public class originalTerminalBlocker extends JFrame {
+    public originalTerminalBlocker() {
+        enableEvents(AWTEvent.WINDOW_EVENT_MASK);
+        this.setSize(new Dimension());
+        this.setTitle("original");
+    }
+
     public static void TerminalBlocker(){
 
     }
