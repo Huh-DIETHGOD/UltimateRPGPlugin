ac4d013551165a730dfc0bdd9dd0d24e94fd1a49
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-07-01 19:28:10 +0800

    term update

--- src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
+++ src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
@@ -2,15 +2,33 @@ package Ultimate.huh.external.replacefiles.terminal;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.WindowEvent;
+import java.awt.Desktop;
+import java.awt.Window;
 
-public class originalTerminalBlocker extends JFrame {
+/**
+ * 通过JFrame enableEvents和processWindowEvents实现原生窗口的关闭
+ */
+public class originalTerminalBlocker extends JFrame{
     public originalTerminalBlocker() {
         enableEvents(AWTEvent.WINDOW_EVENT_MASK);
-        this.setSize(new Dimension());
-        this.setTitle("original");
+        this.setSize(new Dimension(0, 0));
+        this.setTitle(null);
     }
 
-    public static void TerminalBlocker(){
+    protected static Window termTracker(){
+        Window[] window = Desktop.getDesktop().getDesktopWindows();
+        JFrame frame = new JFrame();
 
+        Window tarWindow;
+
+        return tarWindow;
+    }
+
+    protected void TerminalBlocker(WindowEvent event){
+        super.processWindowEvent(event);
+        if (event.getID() == WindowEvent.WINDOW_CLOSING){
+            System.exit(0);
+        }
     }
 }
