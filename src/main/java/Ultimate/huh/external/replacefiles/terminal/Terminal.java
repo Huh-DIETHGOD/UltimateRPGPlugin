515311de5ee7c06dfdf53c1f515f4de672993af4
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-07-02 09:29:52 +0800

    term update

--- src/main/java/Ultimate/huh/external/replacefiles/terminal/Terminal.java
+++ src/main/java/Ultimate/huh/external/replacefiles/terminal/Terminal.java
@@ -1,25 +1,46 @@
 package Ultimate.huh.external.replacefiles.terminal;
 
+import Ultimate.huh.core.UltimateRPGPlugin;
+
 import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+import javax.swing.event.HyperlinkEvent;
+import java.io.File;
 
+/**
+ * 通过JFrame创建URPG控制台
+ */
 public class Terminal extends JFrame{
-
+    private static UltimateRPGPlugin instance;
     public Terminal(){
-        setTitle("Ult-Terminal");
-        setSize(800, 600);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setLayout(new FlowLayout());
-        JButton openListener = new JButton("打开新窗口");
-        add(openListener);
-        openListener.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
+        String htmlPath = instance.getClass().getResource("").getPath() + "/UltimateRPGPlugin/UltTerminal.html";
+        try{
+            setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+            JEditorPane editorPane = new JEditorPane();
+            JScrollPane scrollPane = new JScrollPane(editorPane);
+            editorPane.setContentType("text/html");
+            editorPane.setEditable(false);
+            editorPane.addHyperlinkListener(this::hyperlinkUpdate);
+            File file = new File(htmlPath);
+            String path = file.getAbsolutePath();
+            editorPane.setPage(path);
+        } catch (Exception e){
+        }
+//        setTitle("Ult-Terminal");
+//        setSize(800, 600);
+//        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+//        setLayout(new FlowLayout());
+//        JButton openListener = new JButton("");
+//        add(openListener);
+//        openListener.addActionListener(new ActionListener() {
+//          @Override
+//        public void actionPerformed(ActionEvent e) {
 
-            }
-        });
+//            }
+//        });
+    }
+
+    public void hyperlinkUpdate(HyperlinkEvent e){
+
     }
 
     public void showTerm(){
--- src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
+++ src/main/java/Ultimate/huh/external/replacefiles/terminal/originalTerminalBlocker.java
@@ -1,5 +1,8 @@
 package Ultimate.huh.external.replacefiles.terminal;
 
+import Ultimate.huh.core.UltimateRPGPlugin;
+import org.bukkit.Bukkit;
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.WindowEvent;
@@ -7,28 +10,31 @@ import java.awt.Desktop;
 import java.awt.Window;
 
 /**
- * 通过JFrame enableEvents和processWindowEvents实现原生窗口的关闭
+ * 通过JFrame enableEvents和processWindowEvents实现原生窗口的隐藏
  */
 public class originalTerminalBlocker extends JFrame{
     public originalTerminalBlocker() {
+        Window tarWindow = termTracker();
         enableEvents(AWTEvent.WINDOW_EVENT_MASK);
-        this.setSize(new Dimension(0, 0));
-        this.setTitle(null);
+        tarWindow.setVisible(false);
     }
 
+    /**
+     * 通过Java Swing的getName()函数找寻原生控制台所在窗口并返回
+     * @return
+     */
     protected static Window termTracker(){
-        Window[] window = Desktop.getDesktop().getDesktopWindows();
-        JFrame frame = new JFrame();
-
-        Window tarWindow;
-
-        return tarWindow;
-    }
-
-    protected void TerminalBlocker(WindowEvent event){
-        super.processWindowEvent(event);
-        if (event.getID() == WindowEvent.WINDOW_CLOSING){
-            System.exit(0);
+        //String path = instance.getClass().getResource("").getPath();
+        String core = "spigot-" + Bukkit.getServer().getClass().getPackage().getName().split("\\.")[3] + ".jar";
+        //String termPath = path.replace("plugins", core);
+        Window[] windows = Window.getWindows();
+        Window tarWindow = null;
+        for(Window window : windows) {
+            if (window.getName() != null && window.getName().equals(core)) {
+                tarWindow = window;
+                return tarWindow;
+            }
         }
+        return tarWindow;
     }
 }
