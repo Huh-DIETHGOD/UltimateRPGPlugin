f0e11f33d306577c6f677ca8bd4e8de02c3a47f9
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-07-31 10:28:15 +0800

    tracker 1

--- src/main/java/Ultimate/huh/core/utils/UtilServerStatusTracker.java
+++ src/main/java/Ultimate/huh/core/utils/UtilServerStatusTracker.java
@@ -1,8 +1,151 @@
 package Ultimate.huh.core.utils;
 
-import java.awt.*;
+import org.hyperic.sigar.*;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+import java.util.Map;
+import java.util.Properties;
+
+import static me.clip.placeholderapi.libs.kyori.adventure.internal.properties.AdventureProperties.property;
 
 public class UtilServerStatusTracker {
-    // Track Server Status
+    /**
+     * 检测当前计算机的CPU,内存,网络使用情况
+     */
+    public static void checkServerStatus() {
+        try {
+            property();
+            cpu();
+            memory();
+            os();
+            net();
+            ethernet();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private static void property() throws UnknownHostException {
+        Runtime r = Runtime.getRuntime();
+        Properties props = System.getProperties();
+        InetAddress addr;
+        addr = InetAddress.getLocalHost();
+        String ip = addr.getHostAddress();
+        Map<String, String> map = System.getenv();
+        String userName = map.get("USERNAME");// 获取用户名
+        String computerName = map.get("COMPUTERNAME");// 获取计算机名
+        String userDomain = map.get("USERDOMAIN");// 获取计算机域名
+    }
+
+    private static void memory() throws SigarException {
+        Sigar sigar = new Sigar();
+        Mem mem = sigar.getMem();
+        // 内存总量
+        System.out.println("内存总量:    " + mem.getTotal() / 1024L + "K av");
+        // 当前内存使用量
+        System.out.println("当前内存使用量:    " + mem.getUsed() / 1024L + "K used");
+        // 当前内存剩余量
+        System.out.println("当前内存剩余量:    " + mem.getFree() / 1024L + "K free");
+        Swap swap = sigar.getSwap();
+        // 交换区总量
+        System.out.println("交换区总量:    " + swap.getTotal() / 1024L + "K av");
+        // 当前交换区使用量
+        System.out.println("当前交换区使用量:    " + swap.getUsed() / 1024L + "K used");
+        // 当前交换区剩余量
+        System.out.println("当前交换区剩余量:    " + swap.getFree() / 1024L + "K free");
+    }
+
+    private static void cpu() throws SigarException {
+        Sigar sigar = new Sigar();
+        CpuInfo infos[] = sigar.getCpuInfoList();
+        CpuPerc cpuList[] = null;
+        cpuList = sigar.getCpuPercList();
+        for (int i = 0; i < infos.length; i++) {// 不管是单块CPU还是多CPU都适用
+            CpuInfo info = infos[i];
+            System.out.println("第" + (i + 1) + "块CPU信息");
+            System.out.println("CPU的总量MHz:    " + info.getMhz());// CPU的总量MHz
+            System.out.println("CPU生产商:    " + info.getVendor());// 获得CPU的卖主，如：Intel
+            System.out.println("CPU类别:    " + info.getModel());// 获得CPU的类别，如：Celeron
+            System.out.println("CPU缓存数量:    " + info.getCacheSize());// 缓冲存储器数量
+            printCpuPerc(cpuList[i]);
+        }
+    }
+
+    private static void printCpuPerc(CpuPerc cpu) {
+        System.out.println("CPU用户使用率:    " + CpuPerc.format(cpu.getUser()));// 用户使用率
+        System.out.println("CPU系统使用率:    " + CpuPerc.format(cpu.getSys()));// 系统使用率
+        System.out.println("CPU当前等待率:    " + CpuPerc.format(cpu.getWait()));// 当前等待率
+        System.out.println("CPU当前错误率:    " + CpuPerc.format(cpu.getNice()));//
+        System.out.println("CPU当前空闲率:    " + CpuPerc.format(cpu.getIdle()));// 当前空闲率
+        System.out.println("CPU总的使用率:    " + CpuPerc.format(cpu.getCombined()));// 总的使用率
+    }
+
+    private static void os() {
+        OperatingSystem OS = OperatingSystem.getInstance();
+        // 操作系统内核类型如： 386、486、586等x86
+        System.out.println("操作系统:    " + OS.getArch());
+        System.out.println("操作系统CpuEndian():    " + OS.getCpuEndian());//
+        System.out.println("操作系统DataModel():    " + OS.getDataModel());//
+        // 系统描述
+        System.out.println("操作系统的描述:    " + OS.getDescription());
+        // 操作系统类型
+        // System.out.println("OS.getName():    " + OS.getName());
+        // System.out.println("OS.getPatchLevel():    " + OS.getPatchLevel());//
+        // 操作系统的卖主
+        System.out.println("操作系统的卖主:    " + OS.getVendor());
+        // 卖主名称
+        System.out.println("操作系统的卖主名:    " + OS.getVendorCodeName());
+        // 操作系统名称
+        System.out.println("操作系统名称:    " + OS.getVendorName());
+        // 操作系统卖主类型
+        System.out.println("操作系统卖主类型:    " + OS.getVendorVersion());
+        // 操作系统的版本号
+        System.out.println("操作系统的版本号:    " + OS.getVersion());
+    }
+
+    private static void net() throws Exception {
+        Sigar sigar = new Sigar();
+        String ifNames[] = sigar.getNetInterfaceList();
+        for (int i = 0; i < ifNames.length; i++) {
+            String name = ifNames[i];
+            NetInterfaceConfig ifconfig = sigar.getNetInterfaceConfig(name);
+            System.out.println("网络设备名:    " + name);// 网络设备名
+            System.out.println("IP地址:    " + ifconfig.getAddress());// IP地址
+            System.out.println("子网掩码:    " + ifconfig.getNetmask());// 子网掩码
+            if ((ifconfig.getFlags() & 1L) <= 0L) {
+                System.out.println("!IFF_UP...skipping getNetInterfaceStat");
+                continue;
+            }
+            NetInterfaceStat ifstat = sigar.getNetInterfaceStat(name);
+            System.out.println(name + "接收的总包裹数:" + ifstat.getRxPackets());// 接收的总包裹数
+            System.out.println(name + "发送的总包裹数:" + ifstat.getTxPackets());// 发送的总包裹数
+            System.out.println(name + "接收到的总字节数:" + ifstat.getRxBytes());// 接收到的总字节数
+            System.out.println(name + "发送的总字节数:" + ifstat.getTxBytes());// 发送的总字节数
+            System.out.println(name + "接收到的错误包数:" + ifstat.getRxErrors());// 接收到的错误包数
+            System.out.println(name + "发送数据包时的错误数:" + ifstat.getTxErrors());// 发送数据包时的错误数
+            System.out.println(name + "接收时丢弃的包数:" + ifstat.getRxDropped());// 接收时丢弃的包数
+            System.out.println(name + "发送时丢弃的包数:" + ifstat.getTxDropped());// 发送时丢弃的包数
+        }
+    }
+
+    private static void ethernet() throws SigarException {
+        Sigar sigar = null;
+        sigar = new Sigar();
+        String[] ifaces = sigar.getNetInterfaceList();
+        for (int i = 0; i < ifaces.length; i++) {
+            NetInterfaceConfig cfg = sigar.getNetInterfaceConfig(ifaces[i]);
+            if (NetFlags.LOOPBACK_ADDRESS.equals(cfg.getAddress()) || (cfg.getFlags() & NetFlags.IFF_LOOPBACK) != 0
+                    || NetFlags.NULL_HWADDR.equals(cfg.getHwaddr())) {
+                continue;
+            }
+            System.out.println(cfg.getName() + "IP地址:" + cfg.getAddress());// IP地址
+            System.out.println(cfg.getName() + "网关广播地址:" + cfg.getBroadcast());// 网关广播地址
+            System.out.println(cfg.getName() + "网卡MAC地址:" + cfg.getHwaddr());// 网卡MAC地址
+            System.out.println(cfg.getName() + "子网掩码:" + cfg.getNetmask());// 子网掩码
+            System.out.println(cfg.getName() + "网卡描述信息:" + cfg.getDescription());// 网卡描述信息
+            System.out.println(cfg.getName() + "网卡类型" + cfg.getType());//
+        }
+    }
 
 }
