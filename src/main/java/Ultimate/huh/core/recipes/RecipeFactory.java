84776c5bf077a86d7a5a1acd2b3a3ce92559e740
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-08-05 22:13:11 +0800

    recipes

--- src/main/java/Ultimate/huh/core/recipes/RecipeFactory.java
+++ src/main/java/Ultimate/huh/core/recipes/RecipeFactory.java
@@ -1,10 +1,49 @@
 package Ultimate.huh.core.recipes;
 
+import Ultimate.huh.core.recipes.impl.KeyFactory;
 import Ultimate.huh.core.recipes.impl.Recipe;
 import org.bukkit.NamespacedKey;
+import org.bukkit.configuration.file.YamlConfiguration;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 
 public class RecipeFactory extends Recipe {
+    KeyFactory keyFactory = new KeyFactory();
+
     public RecipeFactory(String name, Object[] recipe, Object result, NamespacedKey key) {
         super(name, recipe, result, key);
+    }
+
+    public Recipe returenRecipe(){
+        Recipe recipe = this.readRecipes();
+        keyFactory.createKeys(recipe.getFile());
+        return this;
+    }
+
+    public static Recipe readRecipes() {
+        // 遍历并读取recipes路径下所有yml文件
+        String filepath = "plugins/UltimateRPG/recipes/recipes.yml";
+        File file = new File(filepath);
+        if (!file.exists()) {
+            return null;
+        }
+
+        try (InputStream inputStream = new FileInputStream(file)) {
+            YamlConfiguration config = new YamlConfiguration();
+            // recipe structure:
+            // ingredients: [a, b, c, a, b, c, a, b, c]
+            // result: d
+            String ingredientsStr = config.getString("ingredients");
+            String resultStr = config.getString("result");
+
+            String[] ingredients = ingredientsStr.split(",");
+            return new Recipe(file.getName(), ingredients, resultStr);
+        } catch (IOException e) {
+            return null;
+        }
+
     }
 }
--- src/main/java/Ultimate/huh/core/recipes/impl/Recipe.java
+++ src/main/java/Ultimate/huh/core/recipes/impl/Recipe.java
@@ -2,6 +2,8 @@ package Ultimate.huh.core.recipes.impl;
 
 import org.bukkit.NamespacedKey;
 
+import java.io.File;
+
 public class Recipe {
 
     private String name;
@@ -20,6 +22,11 @@ public class Recipe {
         this.recipe = recipe;
         this.result = result;
         this.key = key;
+    }
+
+    public File getFile(){
+        String filepath = "plugins/UltimateRPG/recipes/recipes.yml";
+        return new File(filepath);
     }
 
     public String getName() {
