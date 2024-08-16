39e52ae4156599bcf5ebf9c14db401e60a2aba33
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-08-16 12:54:07 +0800

    API update

--- src/main/java/Ultimate/huh/core/expansion/PlayerExpansion.java
+++ src/main/java/Ultimate/huh/core/expansion/PlayerExpansion.java
@@ -1,5 +1,6 @@
 package Ultimate.huh.core.expansion;
 
+import Ultimate.huh.core.state.intellgence.Intelligence;
 import org.bukkit.entity.Player;
 
 
@@ -7,4 +8,8 @@ public interface PlayerExpansion extends Player {
     /**
      * Returns the Intelligence of the player
      */
+    static Intelligence getIntelligence(){
+        return null;
+    }
+
 }
