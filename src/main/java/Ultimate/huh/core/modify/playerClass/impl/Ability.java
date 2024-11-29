704b76c0dbb01754a18185ddbea9a94775897e4d
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2024-11-30 01:02:00 +0800

    ability

---
+++ src/main/java/Ultimate/huh/core/modify/playerClass/impl/Ability.java
@@ -0,0 +1,8 @@
+package Ultimate.huh.core.modify.playerClass.impl;
+
+public class Ability {
+    public Ability(String name, int cooldown, int duration, float manaCost, float healthCost) {
+
+
+    }
+}
--- src/main/java/Ultimate/huh/core/modify/playerClass/impl/AbilityFactory.java
+++ src/main/java/Ultimate/huh/core/modify/playerClass/impl/AbilityFactory.java
@@ -1,4 +1,15 @@
 package Ultimate.huh.core.modify.playerClass.impl;
 
+import org.jetbrains.annotations.Nullable;
+
 public class AbilityFactory {
+    public AbilityFactory(){}
+    public Ability createAbility(
+            String name,@Nullable int cooldown,
+            @Nullable int duration, @Nullable float manaCost, @Nullable float healthCost){
+
+
+
+        return new Ability(name, cooldown, duration, manaCost, healthCost);
+    }
 }
