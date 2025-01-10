9c2be7cfae3ded9a6b23d23dd4823ebb05b555fe
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2025-01-10 21:04:14 +0800

    health update

--- src/main/java/Ultimate/huh/core/modify/state/health/Health.java
+++ src/main/java/Ultimate/huh/core/modify/state/health/Health.java
@@ -3,9 +3,16 @@ package Ultimate.huh.core.modify.state.health;
 import org.bukkit.entity.Entity;
 import org.bukkit.metadata.Metadatable;
 
-public abstract class Health implements Entity, Metadatable {
+public abstract class Health implements Entity {
 
     private float health;
+    public float getHealth() {
+        return health;
+    }
+
+    public void setHealth(float health) {
+        this.health = health;
+    }
 
 
 }
