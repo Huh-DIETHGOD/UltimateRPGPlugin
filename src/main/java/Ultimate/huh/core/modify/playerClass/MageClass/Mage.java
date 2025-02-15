42490522382ca04af28a4471ddcd8e68e05b2968
Huh <118102192+Huh-DIETHGOD@users.noreply.github.com>
2025-02-15 23:57:45 +0800

    listener register bug fixed

--- src/main/java/Ultimate/huh/core/modify/playerClass/MageClass/Mage.java
+++ src/main/java/Ultimate/huh/core/modify/playerClass/MageClass/Mage.java
@@ -3,4 +3,10 @@ package Ultimate.huh.core.modify.playerClass.MageClass;
 import Ultimate.huh.core.modify.playerClass.impl.ClassFactory;
 
 public abstract class Mage extends ClassFactory{
+    public void leftClickAbility() {
+        AbilityLaserBeam laserBeam = new AbilityLaserBeam();
+        laserBeam.laser();
+        // I am tired
+        // swap to mageaddons mod
+    }
 }
